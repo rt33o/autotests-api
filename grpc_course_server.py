46bfc6c75366d9bf3288363181bf59b0a6508a5b
rt33o <krylofskI@gmail.com>
2025-05-21 21:19:22 +0300

    Реализация сервиса, клиента и сервера gRPC v2

--- grpc_course_server.py
+++ grpc_course_server.py
@@ -15,17 +15,7 @@ class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
         )
 
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    course_service_pb2_grpc.add_CourseServiceServicer_to_server(CourseServiceServicer(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    print("gRPC сервер запущен на порту 50051...")
-    try:
-        while True:
-            time.sleep(86400)
-    except KeyboardInterrupt:
-        server.stop(0)
+
 
 
 if __name__ == '__main__':
